5e2462d578c5ee3aa10106ec535288c9ddf4cb61
Tomas Miguez <tmiguez@fu.do>
2022-11-03 15:57:09 -0300

    feat: first working version

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/go-git/go-git/v5"
-  "github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
-func exPath() (string) {
+func exPath() string {
 	ex, err := os.Executable()
 	if err != nil {
 		panic(err)
@@ -20,25 +21,52 @@ func exPath() (string) {
 
 var dir = flag.String("dir", exPath(), "directory of the repository")
 
+type Interval struct {
+	From time.Time
+	To   time.Time
+}
+
+func (interval Interval) duration() time.Duration {
+	return interval.To.Sub(interval.From)
+}
+
+func fmtDuration(duration time.Duration) string {
+	days := duration.Hours() / 24
+
+	return fmt.Sprintf("%.f", days)
+}
+
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  repo, err := git.PlainOpen(*dir)
-  if err != nil {
-    panic(err)
-  }
+	repo, err := git.PlainOpen(*dir)
+	if err != nil {
+		panic(err)
+	}
 
-  cIter, err := repo.Log(&git.LogOptions{})
-  if err != nil {
-    panic(err)
-  }
+	cIter, err := repo.Log(&git.LogOptions{})
+	if err != nil {
+		panic(err)
+	}
 
-  var cCount int
-  err = cIter.ForEach(func(c *object.Commit) error {
-		cCount++
+	intervals := make(map[string]Interval)
+	err = cIter.ForEach(func(c *object.Commit) error {
+		if entry, prs := intervals[c.Author.Email]; !prs {
+			intervals[c.Author.Email] = Interval{From: c.Author.When, To: c.Author.When}
+		} else {
+			if c.Author.When.Before(entry.From) {
+				entry.From = c.Author.When
+			}
+			if c.Author.When.After(entry.To) {
+				entry.To = c.Author.When
+			}
+			intervals[c.Author.Email] = entry
+		}
 
 		return nil
 	})
-
-  fmt.Println(cCount)
+	// .Format("2006/01/02")
+	for name, interval := range intervals {
+		fmt.Println(name, ": ", fmtDuration(interval.duration()), " (", interval.From.Format("2006/01/02"), " ", interval.To.Format("2006/01/02"), ")")
+	}
 }
